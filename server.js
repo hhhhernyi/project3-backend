e0f2b17bbbf6b7d7d078709c6f50261dd2c7f1be
Hern Yi Lee <hyileenet@gmail.com>
2025-02-16 20:15:02 +0800

    setup done with folders

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
+// Import routers
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
@@ -18,6 +19,8 @@ mongoose.connection.on('connected', () => {
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
+
+// Routes
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
