31368317a182394a8db383f5b770828e894bfb05
Hern Yi Lee <hyileenet@gmail.com>
2025-02-17 12:40:36 +0800

    creaeted client model

---
+++ models/clients.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose'); // import mongoose
+
+// define schema
+const clientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    handphoneNumber: {
+        type: String,
+        required: true
+    },
+    priority: {
+        type: String,
+        required: true,
+        enum: ['High', 'Medium', 'Low']
+    },
+    comments: {
+        type: String
+    },
+    agent: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    existingProducts: { type: mongoose.Schema.Types.ObjectId, ref: 'Products' },
+    productsToSell: { type: mongoose.Schema.Types.ObjectId, ref: 'Products' },
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model('Client', clientSchema); // convert the schema to model and export it
