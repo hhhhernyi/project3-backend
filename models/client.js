e6bc8a5ddd263e3d91187061a51d834eef2afb9f
Hern Yi Lee <hyileenet@gmail.com>
2025-02-21 17:45:06 +0800

    edit client model

--- models/client.js
+++ models/client.js
@@ -10,15 +10,36 @@ const clientSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    annualIncome: {
+        type: Number,
+        required: true
+    },
     priority: {
         type: String,
-  
+        required: true,
         enum: ['High', 'Medium', 'Low']
     },
     comments: {
         type: String
     },
-    pipeline:[],
+    dateOfBirth: {
+        type: Date,
+        required: true,
+        trim: true,
+    },
+    occupation: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    existingPlans: {
+        type: [String],
+        required: true,
+        enum: ["Life", "Hospitalization", "Accident", "Investment", "Endowment"],
+    },
     agent: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     productsToSell: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Products' }],
 },
